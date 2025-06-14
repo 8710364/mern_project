9314c95965686880c4857696f088c29a3ab660ed
8710364 <166097571+8710364@users.noreply.github.com>
2025-06-14 20:52:28 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://hospital-system-backend-ui5z.onrender.com/api/v1/user/patient/me",
+          "http://localhost:5000/api/v1/user/patient/me",
           {
             withCredentials: true,
           }
